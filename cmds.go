680e827b574ffa0e8ab35c95c0c6255d7bda0e06
fld <fld@users.noreply.github.com>
2015-11-16 11:09:46 +0200

    Update cmds.go

--- cmds.go
+++ cmds.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -15,67 +14,13 @@ import (
 var lastchat int64 = 0
 
 func CmdInterpreter(channel string, username string, usermessage string) {
-	//regexp we are using to find codes
-	reg := "([[:xdigit:]]{4})-0000-00([[:xdigit:]]{2})-([[:xdigit:]]{4})"
-	re := regexp.MustCompile(reg)
-	matched, _ := regexp.MatchString(reg, usermessage)
-
-	if matched {
-		levelid := re.FindString(usermessage)
-		//Remove the levelid from the message
-		msg := strings.Replace(usermessage, levelid, "", 1)
-		fmt.Printf("Triggered @ %s: %s and %s\n", channel, username, levelid)
-		writeLevelDB(channel, username, msg, levelid)
-	} else if username == "twitchnotify" && !strings.Contains(usermessage, "subscribed to") {
-		split := strings.Split(usermessage, " ")
-		var name string
-		var months string
-		if strings.HasSuffix(usermessage, "row!") {
-			name = split[0]
-			months = split[3]
-		} else if strings.HasSuffix(usermessage, "subscribed!") {
-			name = split[0]
-			months = "1"
-
-		}
-		if name != "" {
-			//fmt.Printf("%q %q\n", name, months)
-			writeSubs(channel, name, months)
-		}
-	} else if strings.Contains(usermessage, nick) || strings.Contains(usermessage, strings.ToLower(nick)) {
+	if strings.Contains(usermessage, nick) || strings.Contains(usermessage, strings.ToLower(nick)) {
 		msg := strings.Replace(usermessage, nick, "", 1)
 		if msg != "" && lastchat+10 <= time.Now().Unix() {
-			//if msg != "" {
 			Message(channel, askOracle(username, msg))
 			lastchat = time.Now().Unix()
 		}
-	} else if strings.HasPrefix(usermessage, "!level") {
-		if isStreamer(username, channel) {
-			message := strings.Replace(usermessage, "!level", "", 1)
-			Message(channel, getLevel(true, channel, message))
-		} else {
-			Message(channel, getLevel(false, channel, ""))
-		}
-	} else if strings.HasPrefix(usermessage, "!reroll") {
-		if isStreamer(username, channel) {
-			Message(channel, doReroll(channel))
-		}
-	} else if strings.HasPrefix(usermessage, "!skip") {
-		if isStreamer(username, channel) {
-			message := strings.Replace(usermessage, "!skip", "", 1)
-			Message(channel, doSkip(channel, message))
-		}
-	} else if strings.HasPrefix(usermessage, "!mariostats") {
-		Message(channel, getStats(channel))
 	}
-}
-
-func isStreamer(username string, channel string) bool {
-	temp := strings.Replace(channel, "#", "", 1)
-	if temp == strings.ToLower(username) {
-		return true
-	}
-	return false
 }
 
 func askOracle(username string, message string) string {
